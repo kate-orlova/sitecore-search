1d6101c7174d432fe8c0753c9c4509b1f5e41c81
Kate Orlova <kate.s.orlova@gmail.com>
2023-09-22 18:12:59 +0100

    Documentation about Source creation

--- src/pages/Configuration.jsx
+++ src/pages/Configuration.jsx
@@ -13,7 +13,20 @@ const ConfigPageSection = styled(PageSection)`
 
 const Configuration = () => {
   return ( 
-    <ConfigPageSection><p>TBC</p></ConfigPageSection>
+    <ConfigPageSection>
+	
+<h1>Search Configuration</h1>
+<h2>How to create a content index in the Sitecore Search?</h2>
+<p>In the Sitecore Search, you define your content index by creating a Source. A Source is a configuration setting that tells what content you would like to make searchable in the Sitecore Search. You can create multiple Sources within your Sitecore Search domain to access content from different locations such as your XML Sitemap, third party APIs, Sitecore Items, database, and various assets in different formats (HTML, PDF, Excel, Word, JSON etc.).</p>
+<p>Once a Source is configured as expected, the Sitecore Search will create indexes and index documents behind the scenes, i.e. you do not need to worry neither about the actual physical index as the Sitecore Search will take a good care of it for you.</p>
+
+<h2>How to configure a Source?</h2>
+<p>To create and manage Sources go to the Sources section in the Customer Engagement Console (CEC). While configuring a new Source you have to specify the following settings:</p>
+
+<h3>How to push your custom data to the Sitecore Search over API?</h3>
+<p>TBC</p>
+
+</ConfigPageSection>
   );
 };
 
