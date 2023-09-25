e93c8368ed7d1635b1764ec15ff9b0527cdeba1d
Kate Orlova <kate.s.orlova@gmail.com>
2023-09-25 17:35:32 +0100

    Connector options for Source creation

--- src/pages/Configuration.jsx
+++ src/pages/Configuration.jsx
@@ -25,9 +25,16 @@ const Configuration = () => {
 <ul>
 <li>SOURCE NAME - enter a name for the source, for example, “Product data push over API”.</li>
 <li>DESCRIPTION - enter a few lines to describe the source you want to configure, for instance, “Push the product catalogue from an external API”.</li>
-<li>CONNECTOR - select the most relevant type of source from the available options:</li>
+<li>CONNECTOR - select the most relevant type of source from the available options:
+	<ul>
+	<li>API Crawler - if your content can only be accessed by an API endpoint and the API returns a JSON response;</li>
+	<li>API Push – add, update or delete the individual documents in a new index or one of  the already existing indexes;</li>
+	<li>Web Crawler - if you have content in one locale and all the content is accessible through a web page;</li>
+	<li>Web Crawler (Advanced) - if you need to index content in multiple languages or want to use JavaScript to extract attributes.</li>
+	</ul>
+</li>
 </ul>
-
+<p>Then click on the <strong>Save</strong> button, if there are no errors, the Sitecore Search creates a new source.</p>
 
 
 <h3>How to push your custom data to the Sitecore Search over API?</h3>
